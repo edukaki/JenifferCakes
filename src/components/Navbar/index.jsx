896ef4df279cd87f9e74e8316b05bd200708c816
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-02-21 21:19:07 +0900

    feat: create navbar

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,32 @@
+'use client';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
+import logo from '../../assets/images/icons/Jennifer-logo.png';
+import hamburguer from '../../assets/images/icons/menu-hamburger.png';
+export default function Navbar() {
+  const [menuHamburguer, setMenuHamburguer] = useState(false);
+  return (
+    <nav className="relative flex flex-row items-center justify-between p-4 bg-white">
+      <Image src={logo} alt="logo" />
+      <ul
+        className={`${
+          menuHamburguer ? '' : 'hidden'
+        } absolute top-full right-0 space-y-8 py-10 w-full bg-white text-center`}
+      >
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/galeria">Galeria</Link>
+        </li>
+        <li>
+          <Link href="/cardapio">Cardápio</Link>
+        </li>
+      </ul>
+      <button onClick={() => setMenuHamburguer(!menuHamburguer)}>
+        <Image src={hamburguer} alt="menu" />
+      </button>
+    </nav>
+  );
+}
