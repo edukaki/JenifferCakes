4a815687c35bb0f55cbcd754ef051419f7eb24d7
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-02-23 12:08:19 +0900

    refactor: carousel to reuse component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,49 +1,27 @@
 'use client';
-import Image from 'next/image';
 import carousel1 from '../assets/images/mobile/carousel1_head.png';
 import carousel2 from '../assets/images/mobile/carousel2_head.png';
 import carousel3 from '../assets/images/mobile/carousel3_head.png';
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import Button from '@/components/Button';
+import profile from '../assets/images/mobile/jeniffer-profile.png';
+import Carousel from '@/components/Carousel';
 
 export default function Home() {
   return (
-    <header className="container-mobile flex flex-col items-center">
-      <h1 className="pt-4 text-center">Bolos feitos com amor</h1>
-      <Carousel
-        className="rounded-lg py-8"
-        autoPlay={true}
-        showThumbs={false}
-        emulateTouch={true}
-        showArrows={false}
-        showStatus={false}
-        infiniteLoop={true}
-        renderIndicator={(onClickHandler, isSelected, index) => {
-          return (
-            <span
-              className={`w-3 h-3 ${
-                isSelected ? 'bg-candy-green' : 'bg-slate-400'
-              } inline-block rounded-full cursor-pointer mx-3`}
-              onClick={onClickHandler}
-              onKeyDown={onClickHandler}
-              key={index}
-              role="button"
-            />
-          );
-        }}
-      >
-        <div>
-          <Image src={carousel1} alt="Fatia de bolo" />
-        </div>
-        <div>
-          <Image src={carousel2} alt="Fatia de bolo" />
-        </div>
-        <div>
-          <Image src={carousel3} alt="Fatia de bolo" />
-        </div>
-      </Carousel>
-      <Button alternative text="Ver cardápio" href="/cardapio" />
+    <header className="h-[calc(100vh-100px)] flex flex-row items-center">
+      <div className="container-mobile flex flex-col items-center">
+        <h1 className="text-center font-bold">Bolos feitos com amor</h1>
+        <Carousel
+          arrImage={[carousel1, carousel2, carousel3]}
+          alt="Fatia de bolo"
+        />
+        <Button
+          alternative
+          text="Ver cardápio"
+          href="/cardapio"
+          margin="mt-0"
+        />
+      </div>
     </header>
   );
 }
---
+++ src/components/Carousel/index.jsx
@@ -0,0 +1,42 @@
+import { Carousel as ImgViewer } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import PropTypes from 'prop-types';
+import Image from 'next/image';
+
+export default function Carousel(props) {
+  return (
+    <ImgViewer
+      className="rounded-lg py-9"
+      autoPlay={true}
+      showThumbs={false}
+      emulateTouch={true}
+      showArrows={false}
+      showStatus={false}
+      infiniteLoop={true}
+      renderIndicator={(onClickHandler, isSelected, index) => {
+        return (
+          <span
+            className={`w-3 h-3 ${
+              isSelected ? 'bg-candy-green' : 'bg-slate-400'
+            } inline-block rounded-full cursor-pointer mx-3`}
+            onClick={onClickHandler}
+            onKeyDown={onClickHandler}
+            key={index}
+            role="button"
+          />
+        );
+      }}
+    >
+      {props.arrImage.map((img, index) => (
+        <div key={index}>
+          <Image src={img} alt={props.alt} />
+        </div>
+      ))}
+    </ImgViewer>
+  );
+}
+
+Carousel.propTypes = {
+  arrImage: PropTypes.array,
+  alt: PropTypes.string,
+};
