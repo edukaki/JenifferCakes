3dadb6cd0fd651de63bc9b18c9a13e72b9696ca0
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-02-24 13:10:25 +0900

    feat: create testimonials object

---
+++ src/assets/utils/testmonials.js
@@ -0,0 +1,22 @@
+import testimonial1 from '@/assets/images/testimonials/testimonial1.png';
+import testimonial2 from '@/assets/images/testimonials/testimonial2.png';
+import testimonial3 from '@/assets/images/testimonials/testimonial3.png';
+
+export const testimonials = [
+  {
+    img: testimonial1,
+    name: 'Luana',
+    quote:
+      'Os bolos da Jeniffer são os mais deliciosos que já comi! Super recomendo!',
+  },
+  {
+    img: testimonial2,
+    name: 'Robson',
+    quote: 'Meu filho se amarrou no bolo de Super Herói que a Jennifer fez!',
+  },
+  {
+    img: testimonial3,
+    name: 'Solange',
+    quote: 'A Jeniffer tem mãos de fada. Os bolos são um capricho!',
+  },
+];
