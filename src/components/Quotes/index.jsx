00179aeb8f23410adce8753ac99a5a5e36148da1
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-02-24 13:13:18 +0900

    refactor(quotes): reusable component

---
+++ src/components/Quotes/index.jsx
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+
+export default function Quotes(props) {
+  return (
+    <div className="flex flex-col items-center text-xl w-11/12">
+      <span className="text-7xl -mb-9 text-left w-full">“</span>
+      <p className={`px-4 ${props.secondParagraph && 'pb-8'}`}>
+        {props.firstParagraph}
+      </p>
+      {props.secondParagraph && <p className="px-4">{props.secondParagraph}</p>}
+      <span className="text-7xl -mb-10 text-right w-full">”</span>
+    </div>
+  );
+}
+
+Quotes.propTypes = {
+  firstParagraph: PropTypes.string.isRequired,
+  secondParagraph: PropTypes.string,
+};
