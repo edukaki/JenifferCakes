c5b1c7c30282c2a58d6475e8a72a3c29dd362d45
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-02-24 17:29:09 +0900

    feat(button): add whatsapp icon

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,19 +1,19 @@
 import Link from 'next/link';
 import PropTypes from 'prop-types';
-
 export default function Button(props) {
   return (
     <Link
       href={props.href}
-      className={`py-3 px-8 shadow-lg ${
+      className={`flex flex-row gap-2 items-center py-3 px-8 shadow-lg ${
         props.margin ? props.margin : 'm-auto'
       } ${
         props.alternative
           ? 'bg-candy-green hover:bg-white hover:border-candy-green hover:border'
-          : 'bg-green-600 hover:opacity-70'
-      } `}
+          : 'bg-green-strong hover:opacity-70'
+      } ${!props.alternative && 'text-white font-semibold text-xl'}}`}
     >
       {props.text}
+      {props.whatsapp && <i class="fa-brands fa-whatsapp fa-lg"></i>}
     </Link>
   );
 }
@@ -23,4 +23,5 @@ Button.propTypes = {
   alternative: PropTypes.bool,
   text: PropTypes.string.isRequired,
   href: PropTypes.string.isRequired,
+  whatsapp: PropTypes.bool,
 };
