cfd3c0582f186b650a063b7fcf4a9c3391dc227d
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-02-25 08:34:34 +0900

    feat: create objGallery

---
+++ src/assets/utils/objGallery.js
@@ -0,0 +1,21 @@
+import infantil1 from '@/assets/images/galeria/infantil/carousel/infantil1.png';
+import infantil2 from '@/assets/images/galeria/infantil/carousel/infantil2.png';
+import infantil3 from '@/assets/images/galeria/infantil/carousel/infantil3.png';
+import infantil4 from '@/assets/images/galeria/infantil/carousel/infantil4.png';
+import infantil5 from '@/assets/images/galeria/infantil/carousel/infantil5.png';
+import infantil6 from '@/assets/images/galeria/infantil/carousel/infantil6.png';
+
+export const objGallery = [
+  {
+    infantil: [
+      { img: infantil1 },
+      { img: infantil2 },
+      { img: infantil3 },
+      { img: infantil4 },
+      { img: infantil5 },
+      { img: infantil6 },
+    ],
+    celebracoes: [],
+    casamento: [],
+  },
+];
