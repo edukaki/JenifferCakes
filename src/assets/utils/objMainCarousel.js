77edee5782f90f90e83f35cb84965959bf3f14fa
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-02-24 15:14:33 +0900

    feat: create objMainCarousel

---
+++ src/assets/utils/objMainCarousel.js
@@ -0,0 +1,9 @@
+import carousel1 from '@/assets/images/home/mobile/carousel1_head.png';
+import carousel2 from '@/assets/images/home/mobile/carousel2_head.png';
+import carousel3 from '@/assets/images/home/mobile/carousel3_head.png';
+
+export const objMainCarousel = [
+  { img: carousel1 },
+  { img: carousel2 },
+  { img: carousel3 },
+];
