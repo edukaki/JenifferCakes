8480dda6fbb3d3b58c9a56b001dcc447cfcb2296
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-02-26 15:53:01 +0900

    feat(galeria): add header

--- src/app/galeria/page.jsx
+++ src/app/galeria/page.jsx
@@ -2,26 +2,36 @@
 import { objGallery } from '@/assets/utils/objGallery';
 import Carousel from '@/components/Carousel';
 import ColoredTitle from '@/components/ColoredTitle';
+import Link from 'next/link';
 
 export default function galeria() {
   return (
-    <main>
-      <h1 className="text-center tracking-wider leading-snug pt-4">Galeria</h1>
-      <section className="flex flex-col items-center text-center gap-7">
-        <ColoredTitle bgColor="green" title="Aniversário Infantil" />
-        <Carousel arrImage={objGallery[0].infantil} alt="Bolo infantil" />
-      </section>
-      <section className="flex flex-col items-center text-center gap-7">
-        <ColoredTitle bgColor="green" title="Aniversário e Celebrações" />
-        <Carousel
-          arrImage={objGallery[0].celebracoes}
-          alt="Bolo de Celebrações"
-        />
-      </section>
-      <section className="flex flex-col items-center text-center gap-7">
-        <ColoredTitle bgColor="green" title="Casamento e Bodas" />
-        <Carousel arrImage={objGallery[0].casamento} alt="Bolo de casamento" />
-      </section>
-    </main>
+    <>
+      <header>
+        <h1 className="text-center tracking-wider leading-snug py-9">
+          Galeria
+        </h1>
+      </header>
+      <main>
+        <section className="flex flex-col items-center text-center gap-7">
+          <ColoredTitle bgColor="green" title="Aniversário Infantil" />
+          <Carousel arrImage={objGallery[0].infantil} alt="Bolo infantil" />
+        </section>
+        <section className="flex flex-col items-center text-center gap-7">
+          <ColoredTitle bgColor="green" title="Aniversário e Celebrações" />
+          <Carousel
+            arrImage={objGallery[0].celebracoes}
+            alt="Bolo de Celebrações"
+          />
+        </section>
+        <section className="flex flex-col items-center text-center gap-7">
+          <ColoredTitle bgColor="green" title="Casamento e Bodas" />
+          <Carousel
+            arrImage={objGallery[0].casamento}
+            alt="Bolo de casamento"
+          />
+        </section>
+      </main>
+    </>
   );
 }
