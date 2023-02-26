fcff0a72e2c4ccf23dac4cfac7d4b33def33db10
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-02-26 15:54:15 +0900

    feat(cardapio): add header

--- src/app/cardapio/page.jsx
+++ src/app/cardapio/page.jsx
@@ -1,3 +1,11 @@
 export default function cardapio() {
-  return <div>cardapio</div>;
+  return (
+    <>
+      <header>
+        <h1 className="text-center tracking-wider leading-snug py-9">
+          Cardápio
+        </h1>
+      </header>
+    </>
+  );
 }
