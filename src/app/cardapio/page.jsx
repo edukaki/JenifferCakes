6f973f3827e0b835ca80854d4b7a574b53a1f74e
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-02-28 20:56:00 +0900

    feat(cardapio): add pay options section

--- src/app/cardapio/page.jsx
+++ src/app/cardapio/page.jsx
@@ -1,6 +1,8 @@
 import FOrder from '@/components/Form/FOrder';
 import Image from 'next/image';
 import phoneICon from '@/assets/images/icons/phone-icon.svg';
+import pixIcon from '@/assets/images/icons/pix-icon.svg';
+import cashIcon from '@/assets/images/icons/cash-icon.svg';
 export default function cardapio() {
   return (
     <>
@@ -11,16 +13,25 @@ export default function cardapio() {
       </header>
       <main>
         <FOrder />
-        <section className="flex flex-col -mt-8 gap-5 text-center text-lg">
+        <section className="flex flex-col -mt-10 gap-5 text-center text-lg">
           <span className="font-bold">Ou Ligue</span>
           <span>
             <Image
               className="inline-block mr-4"
               src={phoneICon}
               alt="phone icon"
+              width={40}
+              height={40}
             />
             (18) 99639-4369
           </span>
+        </section>
+        <section className="grid grid-cols-2 gap-5 -mt-10 place-items-center">
+          <h3 className="text-2xl col-span-2">Formas de pagamento</h3>
+          <Image src={pixIcon} alt="pix icon" width={40} height={40} />
+          <Image src={cashIcon} alt="cash icon" width={40} height={40} />
+          <span>pix</span>
+          <span>dinheiro</span>
         </section>
       </main>
     </>
