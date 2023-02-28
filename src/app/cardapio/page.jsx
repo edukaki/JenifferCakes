578f64cb1f05ea1d5adf332d4fc31667b486c8c9
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-02-28 20:40:05 +0900

    feat(cardapio): add contact section

--- src/app/cardapio/page.jsx
+++ src/app/cardapio/page.jsx
@@ -1,5 +1,6 @@
 import FOrder from '@/components/Form/FOrder';
-
+import Image from 'next/image';
+import phoneICon from '@/assets/images/icons/phone-icon.svg';
 export default function cardapio() {
   return (
     <>
@@ -10,6 +11,17 @@ export default function cardapio() {
       </header>
       <main>
         <FOrder />
+        <section className="flex flex-col -mt-8 gap-5 text-center text-lg">
+          <span className="font-bold">Ou Ligue</span>
+          <span>
+            <Image
+              className="inline-block mr-4"
+              src={phoneICon}
+              alt="phone icon"
+            />
+            (18) 99639-4369
+          </span>
+        </section>
       </main>
     </>
   );
