c9395b0a940d6b5c7b38d63ba87fd4cc98ee0a5a
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-03-01 18:42:41 +0900

    fix: hamburger menu not working properly

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -7,10 +7,7 @@ import logo from '../../assets/images/icons/Jennifer-logo.png';
 export default function Navbar() {
   const [menuHamburguer, setMenuHamburguer] = useState(false);
   return (
-    <nav
-      className="relative flex flex-row items-center justify-between p-4 bg-white z-50"
-      onBlur={() => setMenuHamburguer(false)}
-    >
+    <nav className="relative flex flex-row items-center justify-between p-4 bg-white z-50">
       <Image src={logo} alt="logo" />
       <ul
         className={`${
