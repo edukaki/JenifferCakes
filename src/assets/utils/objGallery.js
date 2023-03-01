c65392cd60e94c7a050c067f8817e436b39a5026
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-03-01 18:35:56 +0900

    feat: feed objGallery

--- src/assets/utils/objGallery.js
+++ src/assets/utils/objGallery.js
@@ -4,6 +4,18 @@ import infantil3 from '@/assets/images/galeria/infantil/carousel/infantil3.png';
 import infantil4 from '@/assets/images/galeria/infantil/carousel/infantil4.png';
 import infantil5 from '@/assets/images/galeria/infantil/carousel/infantil5.png';
 import infantil6 from '@/assets/images/galeria/infantil/carousel/infantil6.png';
+import celebracoes1 from '@/assets/images/galeria/celebracoes/carousel/celebracoes1.png';
+import celebracoes2 from '@/assets/images/galeria/celebracoes/carousel/celebracoes2.png';
+import celebracoes3 from '@/assets/images/galeria/celebracoes/carousel/celebracoes3.png';
+import celebracoes4 from '@/assets/images/galeria/celebracoes/carousel/celebracoes4.png';
+import celebracoes5 from '@/assets/images/galeria/celebracoes/carousel/celebracoes5.png';
+import celebracoes6 from '@/assets/images/galeria/celebracoes/carousel/celebracoes6.png';
+import casamento1 from '@/assets/images/galeria/casamento/carousel/casamento1.png';
+import casamento2 from '@/assets/images/galeria/casamento/carousel/casamento2.png';
+import casamento3 from '@/assets/images/galeria/casamento/carousel/casamento3.png';
+import casamento4 from '@/assets/images/galeria/casamento/carousel/casamento4.png';
+import casamento5 from '@/assets/images/galeria/casamento/carousel/casamento5.png';
+import casamento6 from '@/assets/images/galeria/casamento/carousel/casamento6.png';
 
 export const objGallery = [
   {
@@ -15,7 +27,21 @@ export const objGallery = [
       { img: infantil5 },
       { img: infantil6 },
     ],
-    celebracoes: [],
-    casamento: [],
+    celebracoes: [
+      { img: celebracoes1 },
+      { img: celebracoes2 },
+      { img: celebracoes3 },
+      { img: celebracoes4 },
+      { img: celebracoes5 },
+      { img: celebracoes6 },
+    ],
+    casamento: [
+      { img: casamento1 },
+      { img: casamento2 },
+      { img: casamento3 },
+      { img: casamento4 },
+      { img: casamento5 },
+      { img: casamento6 },
+    ],
   },
 ];
