94725b86bf86fb8e391815b7f7ad586b540bf549
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-03-02 21:12:52 +0900

    feat(cardMain): update element color

--- src/components/CardMain/index.jsx
+++ src/components/CardMain/index.jsx
@@ -2,26 +2,26 @@ import PropTypes from 'prop-types';
 import Image from 'next/image';
 
 export default function CardMain(props) {
+  const color =
+    props.color === 'green'
+      ? 'bg-candy-green'
+      : props.color === 'pink'
+      ? 'bg-candy-pink'
+      : '';
   return (
     <section className="flex flex-col justify-center min-h-screen text-center relative">
       <div className="absolute grid grid-cols-2 h-full w-full -z-50">
         <div
           className={`${
             props.left ? 'col-start-1' : props.right ? 'col-start-2' : ''
-          } ${
-            props.color === 'green'
-              ? 'bg-candy-green'
-              : props.color === 'pink'
-              ? 'bg-candy-pink'
-              : ''
-          }`}
+          } ${color}`}
         ></div>
       </div>
       <h2 className="pt-4 pb-11">{props.title}</h2>
       <Image className="w-full" src={props.src} alt={props.alt} />
       <div className="flex flex-col items-center p-5 mt-16 mb-10 bg-light-beige">
         <h3 className="tracking-wider">{props.subtitle}</h3>
-        <div className="w-28 h-1 my-4 rounded-full bg-candy-green" />
+        <div className={`w-28 h-1 my-4 rounded-full ${color}`} />
         <p className="w-11/12">{props.text}</p>
       </div>
     </section>
