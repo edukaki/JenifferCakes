621e91f58bed30b6a68e45fe466e5db4be2786f7
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-03-04 08:05:28 +0900

    feat(objMainCarousel): update imports

--- src/assets/utils/objMainCarousel.js
+++ src/assets/utils/objMainCarousel.js
@@ -1,6 +1,6 @@
-import carousel1 from '@/assets/images/home/mobile/carousel1_head.png';
-import carousel2 from '@/assets/images/home/mobile/carousel2_head.png';
-import carousel3 from '@/assets/images/home/mobile/carousel3_head.png';
+import carousel1 from '@/assets/images/home/mobile/carousel1_head.jpg';
+import carousel2 from '@/assets/images/home/mobile/carousel2_head.jpg';
+import carousel3 from '@/assets/images/home/mobile/carousel3_head.jpg';
 
 export const objMainCarousel = [
   { img: carousel1 },
