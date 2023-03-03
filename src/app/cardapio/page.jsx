7c6e3d0a007e7d2900b5a6dbf50792e09e972495
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-03-03 17:40:46 +0900

    feat(cardapio): insert address map

--- src/app/cardapio/page.jsx
+++ src/app/cardapio/page.jsx
@@ -13,25 +13,41 @@ export default function cardapio() {
       </header>
       <main>
         <FOrder />
-        <section className="flex flex-col -mt-10 gap-5 text-center text-lg">
-          <span className="font-bold">Ou Ligue</span>
-          <span>
-            <Image
-              className="inline-block mr-4"
-              src={phoneICon}
-              alt="phone icon"
-              width={40}
-              height={40}
-            />
-            (18) 99639-4369
-          </span>
-        </section>
-        <section className="grid grid-cols-2 gap-5 -mt-10 place-items-center">
-          <h3 className="text-2xl col-span-2">Formas de pagamento</h3>
+        <section className="grid grid-cols-2 gap-5 pt-20 pb-12 place-items-center">
+          <h3 className="text-2xl col-span-2 text-center">
+            Formas de pagamento
+          </h3>
           <Image src={pixIcon} alt="pix icon" width={40} height={40} />
           <Image src={cashIcon} alt="cash icon" width={40} height={40} />
           <span>pix</span>
           <span>dinheiro</span>
+        </section>
+        <section className="flex flex-col text-center">
+          <h3 className="text-2xl col-span-2 text-center">Endereço</h3>
+
+          <iframe
+            className="w-full h-[400px] pt-5 pb-2"
+            style={{ border: 0 }}
+            loading="lazy"
+            src={`https://www.google.com/maps/embed/v1/place?q=-22.296151362665796%2C%20-51.38250372938472&key=${process.env.MAPS_API_KEY}`}
+          ></iframe>
+          <p>
+            Rua Antônio Yoshito Matsumoto, 300,
+            <br />
+            Bairro Centro
+            <br />
+            Anhumas - SP, Brasil
+          </p>
+          <span className="mt-5">
+            <Image
+              className="inline-block mr-4"
+              src={phoneICon}
+              alt="phone icon"
+              width={25}
+              height={25}
+            />
+            (18) 99639-4369
+          </span>
         </section>
       </main>
     </>
