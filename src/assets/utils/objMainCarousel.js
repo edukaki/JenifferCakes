659adf088167f2b6b2662f249cd85c8bc0424d17
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-03-04 08:06:05 +0900

    feat(objMainCarousel): create objMainCarouselDesktop

--- src/assets/utils/objMainCarousel.js
+++ src/assets/utils/objMainCarousel.js
@@ -1,9 +1,18 @@
 import carousel1 from '@/assets/images/home/mobile/carousel1_head.jpg';
 import carousel2 from '@/assets/images/home/mobile/carousel2_head.jpg';
 import carousel3 from '@/assets/images/home/mobile/carousel3_head.jpg';
+import carouselDesktop1 from '@/assets/images/home/desktop/carousel1_head.jpg';
+import carouselDesktop2 from '@/assets/images/home/desktop/carousel2_head.jpg';
+import carouselDesktop3 from '@/assets/images/home/desktop/carousel3_head.jpg';
 
-export const objMainCarousel = [
+export const objMainCarouselMobile = [
   { img: carousel1 },
   { img: carousel2 },
   { img: carousel3 },
+];
+
+export const objMainCarouselDesktop = [
+  { img: carouselDesktop1 },
+  { img: carouselDesktop2 },
+  { img: carouselDesktop3 },
 ];
