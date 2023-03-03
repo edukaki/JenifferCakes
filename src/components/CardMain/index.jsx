4e0e5b3b1226b8325b1bd88162edf2aa0c966d5d
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-03-03 19:50:57 +0900

    feat(cardMain): set tablet responsiveness

--- src/components/CardMain/index.jsx
+++ src/components/CardMain/index.jsx
@@ -9,20 +9,26 @@ export default function CardMain(props) {
       ? 'bg-candy-pink'
       : '';
   return (
-    <section className="flex flex-col justify-center min-h-screen text-center relative">
+    <section className="flex flex-col justify-center min-h-screen text-center relative md:min-h-fit md:h-full md:py-24">
       <div className="absolute grid grid-cols-2 h-full w-full -z-50">
         <div
           className={`${
-            props.left ? 'col-start-1' : props.right ? 'col-start-2' : ''
+            props.left
+              ? 'col-start-1 md:col-span-2'
+              : props.right
+              ? 'col-start-2 md:col-span-2'
+              : ''
           } ${color}`}
         ></div>
       </div>
       <h2 className="pt-4 pb-11">{props.title}</h2>
-      <Image className="w-full" src={props.src} alt={props.alt} />
-      <div className="flex flex-col items-center p-5 mt-16 mb-10 bg-light-beige">
-        <h3 className="tracking-wider">{props.subtitle}</h3>
-        <div className={`w-28 h-1 my-4 rounded-full ${color}`} />
-        <p className="w-11/12">{props.text}</p>
+      <Image className="w-full md:px-2" src={props.src} alt={props.alt} />
+      <div className="md:px-4">
+        <div className="flex flex-col items-center p-5 mt-16 mb-10 bg-light-beige md:rounded-md">
+          <h3 className="tracking-wider">{props.subtitle}</h3>
+          <div className={`w-28 h-1 my-4 rounded-full ${color}`} />
+          <p className="w-11/12">{props.text}</p>
+        </div>
       </div>
     </section>
   );
