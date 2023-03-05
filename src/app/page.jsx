4402cb6041b5820e5d15c0bc2b80048fe81fda09
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-03-05 19:16:15 +0900

    feat(index): set desktop responsiveness

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,53 +9,69 @@ import quality from '@/assets/images/home/mobile/quality.png';
 import ColoredTitle from '@/components/ColoredTitle';
 import Quotes from '@/components/Quotes';
 import { objTestimonials } from '@/assets/utils/objTestmonials';
-import { objMainCarousel } from '@/assets/utils/objMainCarousel';
+import {
+  objMainCarouselMobile,
+  objMainCarouselDesktop,
+} from '@/assets/utils/objMainCarousel';
 import callToAction from '@/assets/images/home/mobile/call-to-action-price.png';
+import callToActionDesktop from '@/assets/images/home/desktop/call-to-action-price.jpg';
 
 export default function Home() {
   return (
     <>
-      <header className="h-[calc(100vh-100px)] flex flex-row items-center md:px-20">
-        <div className="flex flex-col items-center">
+      <header className="h-[calc(100vh-100px)] flex flex-row items-center m-auto md:px-20 md:h-full md:mt-8 mb-24">
+        <div className="flex flex-col items-center m-auto relative">
           <h1 className="text-center">Bolos feitos com amor</h1>
-          <Carousel arrImage={objMainCarousel} alt="Fatia de bolo" />
-          <Button
-            alternative
-            text="Ver cardápio"
-            href="/cardapio"
-            margin="mt-0"
+          <Carousel
+            arrImage={
+              window.innerWidth >= 1280
+                ? objMainCarouselDesktop
+                : objMainCarouselMobile
+            }
+            alt="Fatia de bolo"
           />
+          <div className="xl:absolute xl:bottom-1/4">
+            <Button
+              alternative
+              text="Ver cardápio"
+              href="/cardapio"
+              margin="mt-0"
+              absolute
+            />
+          </div>
         </div>
       </header>
       <main className="space-y-24">
-        <section className="flex flex-col items-center text-center gap-7 container-mobile md:px-0">
-          <h1 className="text-center w-9/12 tracking-wider leading-snug">
-            Oi, eu sou a Jeniffer !
-          </h1>
-          <Image
-            className="w-8/12"
-            src={profile}
-            alt="Jeniffer segurando bolo"
-          />
-          <div className="flex flex-col items-center">
-            <Quotes
-              firstParagraph="Trabalho com confeitaria profissional há mais de 2 anos."
-              secondParagraph="O que me dá mais satisfação é ver a alegria no rosto dos meus
-              clientes."
+        <section className="container-mobile xl:max-w-[1280px] xl:m-auto">
+          <div className="flex flex-col items-center text-center gap-7 md:px-0 xl:grid xl:grid-flow-row xl:place-items-center xl:gap-10">
+            <h1 className="text-center w-9/12 tracking-wider leading-snug xl:col-start-2">
+              Oi, eu sou a Jeniffer !
+            </h1>
+            <Image
+              className="w-8/12 xl:row-start-1 xl:row-span-2 xl:min-w-[400px]"
+              src={profile}
+              alt="Jeniffer segurando bolo"
             />
-            <div className="grid grid-cols-2 py-2 mt-6 w-11/12 text-center text-2xl font-semibold md:py-8 md:text-3xl bg-white text-red-strong md:w-full">
-              <span className="p-4 ">
-                1000 +<br />
-                bolos entregues
-              </span>
-              <span className="p-4">
-                500 +<br />
-                clientes felizes
-              </span>
+            <div className="flex flex-col items-center xl:w-[450px]">
+              <Quotes
+                firstParagraph="Trabalho com confeitaria profissional há mais de 2 anos."
+                secondParagraph="O que me dá mais satisfação é ver a alegria no rosto dos meus
+              clientes."
+              />
             </div>
           </div>
         </section>
-        <section className="grid grid-rows-2 gap-24 container-mobile md:grid-rows-1 md:grid-cols-2 md:gap-2">
+        <div className="grid grid-cols-2 py-2 mt-6 w-11/12 m-auto text-center text-2xl font-semibold md:py-8 md:text-3xl bg-white text-red-strong md:w-full">
+          <span className="p-4 ">
+            1000 +<br />
+            bolos entregues
+          </span>
+          <span className="p-4">
+            500 +<br />
+            clientes felizes
+          </span>
+        </div>
+        <section className="grid grid-rows-2 gap-24 container-mobile md:grid-rows-1 md:grid-cols-2 md:gap-2 xl:grid-cols-1">
           <CardMain
             title="Artesanal"
             src={artisan}
@@ -83,14 +99,27 @@ export default function Home() {
           />
           <Carousel alt="testemunha" arrImage={objTestimonials} />
         </section>
-        <section className="flex flex-col items-center gap-12 container-mobile">
+        <section className="flex flex-col items-center container-mobile">
           <ColoredTitle
             bgColor="green"
             title="Saboreie a Felicidade"
             calligraphy
           />
-          <Image src={callToAction} alt="imagem de bolo com oferta" />
-          <Button text="fazer pedido" href="/cardapio" alternative />
+          <div className="flex flex-col items-center gap-12 pt-12 xl:bg-candy-green xl:w-full xl:flex-row xl:p-12 xl:justify-evenly 2xl:justify-center">
+            <Image
+              className="xl:max-h-[600px] 2xl:max-w-[800px] xl:w-auto"
+              src={
+                window.innerWidth >= 1280 ? callToActionDesktop : callToAction
+              }
+              alt="imagem de bolo com oferta"
+            />
+            <Button
+              text="fazer pedido"
+              href="/cardapio"
+              alternative
+              margin="xl:m-0"
+            />
+          </div>
         </section>
       </main>
     </>
