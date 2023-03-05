5e0180d9b8b806b0ffd0b1224676395a63f7a799
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-03-05 19:13:00 +0900

    refactor(footer): create an object to render dynamically

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,6 +5,24 @@ import whatsapp from '@/assets/images/icons/whatsapp-icon.svg';
 import Link from 'next/link';
 
 export default function Footer() {
+  const socials = [
+    {
+      href: 'https://instagram.com/jeniffer_santos_cassajus?igshid=YmMyMTA2M2Y=',
+      src: instagram,
+      alt: 'Instagram logo',
+    },
+    {
+      href: 'https://www.facebook.com/jeniffersantos.santos.75?mibextid=LQQJ4d',
+      src: facebook,
+      alt: 'Facebook logo',
+    },
+    {
+      href: 'https://api.whatsapp.com/send?phone=5518996394369',
+      src: whatsapp,
+      alt: 'WhatsApp logo',
+    },
+  ];
+
   return (
     <footer className="text-center container-mobile">
       <div className="bg-white">
@@ -12,30 +30,17 @@ export default function Footer() {
           Entre em contato
         </h2>
         <ul className="flex flex-row justify-center pt-5 pb-8">
-          <li>
-            <Link
-              target="_blank"
-              href="https://instagram.com/jeniffer_santos_cassajus?igshid=YmMyMTA2M2Y="
-            >
-              <Image src={instagram} alt="Instagram logo" />
-            </Link>
-          </li>
-          <li>
-            <Link
-              target="_blank"
-              href="https://www.facebook.com/jeniffersantos.santos.75?mibextid=LQQJ4d"
-            >
-              <Image className="mx-6" src={facebook} alt="Facebook logo" />
-            </Link>
-          </li>
-          <li>
-            <Link
-              target="_blank"
-              href="https://api.whatsapp.com/send?phone=5518996394369"
-            >
-              <Image src={whatsapp} alt="WhatsApp logo" />
-            </Link>
-          </li>
+          {socials.map((social, index) => (
+            <li className="px-4" key={index}>
+              <Link className="px-14" target="_blank" href={social.href}>
+                <Image
+                  className="w-10 m-auto"
+                  src={social.src}
+                  alt={social.alt}
+                />
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
       <p className="text-xs py-5">
