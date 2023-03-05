52e78c27f3e3c4384290e2d234b6d6e0f3586177
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-03-05 19:11:25 +0900

    feat(quotes): set desktop responsiveness

--- src/components/Quotes/index.jsx
+++ src/components/Quotes/index.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 export default function Quotes(props) {
   return (
-    <div className="flex flex-col items-center text-xl w-11/12 md:w-8/12">
+    <div className="flex flex-col items-center text-xl w-11/12 md:w-8/12 xl:w-fit">
       <span className="text-7xl -mb-9 text-left w-full">“</span>
       <p className={`px-4 ${props.secondParagraph && 'pb-8'} md:text-2xl`}>
         {props.firstParagraph}
