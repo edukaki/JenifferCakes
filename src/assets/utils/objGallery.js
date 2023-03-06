53ce9198713e3fb90090445d043945715dcb054d
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-03-06 20:55:20 +0900

    style: organize the imports

--- src/assets/utils/objGallery.js
+++ src/assets/utils/objGallery.js
@@ -1,21 +1,21 @@
-import infantil1 from '@/assets/images/galeria/infantil/carousel/infantil1.png';
-import infantil2 from '@/assets/images/galeria/infantil/carousel/infantil2.png';
-import infantil3 from '@/assets/images/galeria/infantil/carousel/infantil3.png';
-import infantil4 from '@/assets/images/galeria/infantil/carousel/infantil4.png';
-import infantil5 from '@/assets/images/galeria/infantil/carousel/infantil5.png';
-import infantil6 from '@/assets/images/galeria/infantil/carousel/infantil6.png';
-import celebracoes1 from '@/assets/images/galeria/celebracoes/carousel/celebracoes1.png';
-import celebracoes2 from '@/assets/images/galeria/celebracoes/carousel/celebracoes2.png';
-import celebracoes3 from '@/assets/images/galeria/celebracoes/carousel/celebracoes3.png';
-import celebracoes4 from '@/assets/images/galeria/celebracoes/carousel/celebracoes4.png';
-import celebracoes5 from '@/assets/images/galeria/celebracoes/carousel/celebracoes5.png';
-import celebracoes6 from '@/assets/images/galeria/celebracoes/carousel/celebracoes6.png';
 import casamento1 from '@/assets/images/galeria/casamento/carousel/casamento1.png';
 import casamento2 from '@/assets/images/galeria/casamento/carousel/casamento2.png';
 import casamento3 from '@/assets/images/galeria/casamento/carousel/casamento3.png';
 import casamento4 from '@/assets/images/galeria/casamento/carousel/casamento4.png';
 import casamento5 from '@/assets/images/galeria/casamento/carousel/casamento5.png';
 import casamento6 from '@/assets/images/galeria/casamento/carousel/casamento6.png';
+import celebracoes1 from '@/assets/images/galeria/celebracoes/carousel/celebracoes1.png';
+import celebracoes2 from '@/assets/images/galeria/celebracoes/carousel/celebracoes2.png';
+import celebracoes3 from '@/assets/images/galeria/celebracoes/carousel/celebracoes3.png';
+import celebracoes4 from '@/assets/images/galeria/celebracoes/carousel/celebracoes4.png';
+import celebracoes5 from '@/assets/images/galeria/celebracoes/carousel/celebracoes5.png';
+import celebracoes6 from '@/assets/images/galeria/celebracoes/carousel/celebracoes6.png';
+import infantil1 from '@/assets/images/galeria/infantil/carousel/infantil1.png';
+import infantil2 from '@/assets/images/galeria/infantil/carousel/infantil2.png';
+import infantil3 from '@/assets/images/galeria/infantil/carousel/infantil3.png';
+import infantil4 from '@/assets/images/galeria/infantil/carousel/infantil4.png';
+import infantil5 from '@/assets/images/galeria/infantil/carousel/infantil5.png';
+import infantil6 from '@/assets/images/galeria/infantil/carousel/infantil6.png';
 
 export const objGallery = [
   {
--- src/assets/utils/objMainCarousel.js
+++ src/assets/utils/objMainCarousel.js
@@ -1,9 +1,9 @@
-import carousel1 from '@/assets/images/home/mobile/carousel1_head.jpg';
-import carousel2 from '@/assets/images/home/mobile/carousel2_head.jpg';
-import carousel3 from '@/assets/images/home/mobile/carousel3_head.jpg';
 import carouselDesktop1 from '@/assets/images/home/desktop/carousel1_head.jpg';
 import carouselDesktop2 from '@/assets/images/home/desktop/carousel2_head.jpg';
 import carouselDesktop3 from '@/assets/images/home/desktop/carousel3_head.jpg';
+import carousel1 from '@/assets/images/home/mobile/carousel1_head.jpg';
+import carousel2 from '@/assets/images/home/mobile/carousel2_head.jpg';
+import carousel3 from '@/assets/images/home/mobile/carousel3_head.jpg';
 
 export const objMainCarouselMobile = [
   { img: carousel1 },
