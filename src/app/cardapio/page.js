2b27450d00c8509cd7e0dd5bf54ec93e5d25d1a1
Eduardo <eduardo.arakaki@hotmail.com>
2023-03-07 21:34:02 +0900

    feat(cardapio): set desktop responsiveness

--- src/app/cardapio/page.js
+++ src/app/cardapio/page.js
@@ -26,32 +26,35 @@ export default function cardapio() {
             <span>dinheiro</span>
           </div>
         </section>
-        <section className="flex flex-col text-center">
-          <h3 className="text-2xl col-span-2 text-center">Endereço</h3>
-
-          <iframe
-            className="w-full h-[400px] pt-5 pb-2"
-            style={{ border: 0 }}
-            loading="lazy"
-            src={`https://www.google.com/maps/embed/v1/place?q=-22.296151362665796%2C%20-51.38250372938472&key=${process.env.MAPS_API_KEY}`}
-          ></iframe>
-          <p>
-            Rua Antônio Yoshito Matsumoto, 300,
-            <br />
-            Bairro Centro
-            <br />
-            Anhumas - SP, Brasil
-          </p>
-          <span className="mt-5">
-            <Image
-              className="inline-block mr-4"
-              src={phoneICon}
-              alt="phone icon"
-              width={25}
-              height={25}
-            />
-            (18) 99639-4369
-          </span>
+        <section className="flex flex-col gap-5 text-center">
+          <h3 className="text-2xl text-center">Endereço</h3>
+          <div className="xl:grid xl:grid-cols-2 xl:place-items-center">
+            <iframe
+              className="w-full h-[400px] pt-5 pb-2"
+              style={{ border: 0 }}
+              loading="lazy"
+              src={`https://www.google.com/maps/embed/v1/place?q=-22.296151362665796%2C%20-51.38250372938472&key=${process.env.MAPS_API_KEY}`}
+            ></iframe>
+            <div className="flex flex-col">
+              <p>
+                Rua Antônio Yoshito Matsumoto, 300,
+                <br />
+                Bairro Centro
+                <br />
+                Anhumas - SP, Brasil
+              </p>
+              <span className="mt-5">
+                <Image
+                  className="inline-block mr-4"
+                  src={phoneICon}
+                  alt="phone icon"
+                  width={25}
+                  height={25}
+                />
+                (18) 99639-4369
+              </span>
+            </div>
+          </div>
         </section>
       </main>
     </>
