0f65e57cae49544b337626931a75525b3bb719fd
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-03-07 15:31:42 +0900

    feat(navbar): set desktop rersponsiveness

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -20,41 +20,46 @@ export default function Navbar() {
 
   return (
     <nav
-      className={`flex flex-row w-full items-center justify-between p-4 z-50 bg-white ${
-        navScroll ? 'sticky top-0' : 'relative border-b-2'
+      className={`bg-white z-50 container-mobile ${
+        navScroll ? 'sticky top-0' : 'relative'
       }`}
     >
-      <Link href="/">
-        <Image src={logo} alt="logo" />
-      </Link>
-      <ul
-        className={`${
-          menuHamburguer ? '' : 'hidden'
-        } absolute top-full right-0 space-y-8 py-10 w-full bg-white text-center z-50`}
-      >
-        <li>
-          <Link href="/" onClick={() => setMenuHamburguer(false)}>
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link href="/galeria" onClick={() => setMenuHamburguer(false)}>
-            Galeria
-          </Link>
-        </li>
-        <li>
-          <Link href="/cardapio" onClick={() => setMenuHamburguer(false)}>
-            Cardápio
-          </Link>
-        </li>
-      </ul>
-      <button onClick={() => setMenuHamburguer(!menuHamburguer)}>
-        {!menuHamburguer ? (
-          <i className="fa-solid fa-bars fa-2xl" />
-        ) : (
-          <i className="fa-solid fa-xmark fa-2xl" />
-        )}
-      </button>
+      <div className="relative flex flex-row w-full items-center justify-between p-4 2xl:max-w-[70%] 2xl:mx-auto">
+        <Link href="/">
+          <Image src={logo} alt="logo" />
+        </Link>
+        <ul
+          className={`${
+            menuHamburguer ? '' : 'hidden'
+          } absolute top-full right-0 space-y-8 py-10 w-full bg-white text-center z-50 xl:relative xl:flex xl:flex-row xl:items-center xl:gap-24 xl:text-xl xl:space-y-0 xl:py-0 xl:w-auto xl:justify-center`}
+        >
+          <li>
+            <Link href="/" onClick={() => setMenuHamburguer(false)}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/galeria" onClick={() => setMenuHamburguer(false)}>
+              Galeria
+            </Link>
+          </li>
+          <li>
+            <Link href="/cardapio" onClick={() => setMenuHamburguer(false)}>
+              Cardápio
+            </Link>
+          </li>
+        </ul>
+        <button
+          className="xl:hidden"
+          onClick={() => setMenuHamburguer(!menuHamburguer)}
+        >
+          {!menuHamburguer ? (
+            <i className="fa-solid fa-bars fa-2xl" />
+          ) : (
+            <i className="fa-solid fa-xmark fa-2xl" />
+          )}
+        </button>
+      </div>
     </nav>
   );
 }
