53999ceb8871ae354eb7ae2a013b793c022d0896
Eduardo <eduardo.arakaki@hotmail.com>
2023-03-08 20:53:05 +0900

    fix:(navbar): mobile version with broken style

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -27,11 +27,7 @@ export default function Navbar() {
   }, []);
 
   return (
-    <nav
-      className={`bg-white z-50 container-mobile ${
-        navScroll ? 'sticky top-0' : 'relative'
-      }`}
-    >
+    <nav className={`bg-white z-50 ${navScroll ? 'sticky top-0' : 'relative'}`}>
       <div className="relative flex flex-row w-full items-center justify-between p-4 2xl:max-w-[70%] 2xl:mx-auto">
         <Link href="/">
           <Image src={logo} alt="logo" />
@@ -39,13 +35,13 @@ export default function Navbar() {
         <ul
           className={`${
             menuHamburguer ? '' : 'hidden'
-          } absolute top-full right-0 py-10 w-full bg-white text-center z-50 xl:relative xl:flex xl:flex-row xl:items-center xl:text-xl xl:space-y-0 xl:py-0 xl:w-auto xl:justify-center`}
+          } absolute top-full right-0 space-y-8 py-10 w-full bg-white text-center z-50 xl:relative xl:flex xl:flex-row xl:items-center xl:text-xl xl:space-y-0 xl:py-0 xl:w-auto xl:justify-center`}
         >
           {links.map(({ href, text }, index) => (
             <li key={index}>
               <Link
                 href={href}
-                className={`px-12 py-12 hover:bg-black hover:bg-opacity-20 transition-colors ease-in-out duration-300`}
+                className={`px-12 py-4 hover:bg-black hover:bg-opacity-20 transition-colors ease-in-out duration-300 xl:px-12 xl:py-12`}
                 onClick={() => setMenuHamburguer(false)}
               >
                 <span
